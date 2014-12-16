dfd197adb4b839955f65a417cdfbb13cf727fb50
youngminz <youngmin@youngminz.kr>
2014-12-16 17:37:14 +0900

    Scroll to top when load

--- script/script.coffee
+++ script/script.coffee
@@ -2,4 +2,6 @@ $(document).ready ->
 	$ "#logo"
 		.css "padding-top", Math.floor $(window).height() / 2 - $(window).height() / 3
 		.css "padding-bottom", Math.floor $(window).height() / 2 + 10
+	$ "html, head"
+		.scrollTop(0)
 	return
--- script/script.js
+++ script/script.js
@@ -2,6 +2,7 @@
 (function() {
   $(document).ready(function() {
     $("#logo").css("padding-top", Math.floor($(window).height() / 2 - $(window).height() / 3)).css("padding-bottom", Math.floor($(window).height() / 2 + 10));
+    $("html, head").scrollTop(0);
   });
 
 }).call(this);
